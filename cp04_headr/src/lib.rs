5ffa6d76b1b842d1c8952df143c23734a8fc84c5
kiharito <kihara13611.c7d7@gmail.com>
2024-03-17 22:57:36 +0900

    add test for parse_positive_int

--- cp04_headr/src/lib.rs
+++ cp04_headr/src/lib.rs
@@ -21,3 +21,23 @@ pub fn run(config: Config) -> MyResult<()> {
     dbg!(config);
     Ok(())
 }
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    unimplemented!();
+}
+
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
